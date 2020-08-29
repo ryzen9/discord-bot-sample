aa568a4f24653baecf1a24e8f8b6eb4fa4590ede
ryzen9 <ryzen9.lineagem@gmail.com>
2020-08-30 03:11:54 +0900

    refactoring lineagem_site_reader

---
+++ lineagem_site_reader.js
@@ -0,0 +1,92 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+module.exports = class LineageMSiteReader {
+  static NEWS_TYPE = {
+    news: 0,
+    event: 1,
+    maintenance: 2,
+    trouble: 3,
+    patch: 4,
+    other: 5, 
+  }
+
+  newsName(type) {
+    const name = Object.keys(LineageMSiteReader.NEWS_TYPE)[Object.values(LineageMSiteReader.NEWS_TYPE).indexOf(type)]
+    return type !== LineageMSiteReader.NEWS_TYPE.news ? `news_${name}` : 'news';
+  }
+
+  newsUrl(name) {
+    const BASE_URL = "https://lineagem-jp.com/";
+    return `${BASE_URL}${name}`
+  }
+
+  constructor(newsType) {
+    this.type = Object.keys(LineageMSiteReader.NEWS_TYPE)[Object.values(LineageMSiteReader.NEWS_TYPE).indexOf(newsType)]
+    this.name = this.newsName(newsType);
+    this.url = this.newsUrl(this.name);
+    // console.log(this.type)
+    // console.log(this.name)
+    // console.log(this.url)
+  }
+
+  async getNewsContent(url) {
+    const body = await axios.get(url);
+    return body.data;
+  } 
+
+  async parseNewContent(contentBody) {
+    const $ = cheerio.load(contentBody);
+    const pairs = $('.newsListTable tbody tr').map((i, el) => {
+      return {
+        date: $(el).find('.table__cellDate').text(),
+        new: $(el).find('.table__cellTag span').text(),
+        text: $(el).find('.table__cellTitle a').text(),
+        url: $(el).find('.table__cellTitle a').attr('href'),
+      }
+    });
+    return pairs.toArray();
+  }
+
+  async readOldContent(contentName) {
+    let oldContent = "";
+    try {
+      oldContent = await fs.readFileSync(contentName, 'utf-8');
+    } catch (error) {
+      oldContent = "";
+    }
+    return oldContent;
+  }
+
+  async writeOldContent(contentName, content) {
+    // console.log(contentName)
+    await fs.writeFileSync(contentName, content, 'utf-8');
+  } 
+
+  diffNewsJson(oldContent, newContent) {
+    const result = newContent.filter(newEl => {
+        if (newEl.new !== 'NEW') {
+          return false;
+        }
+        return (oldContent.find(oldEl => newEl.url === oldEl.url) === undefined);
+    }); 
+    return result;
+  }
+
+  async getUpdateNewsInfo() {
+    const contentBody = await this.getNewsContent(this.url);
+    const newContent = await this.parseNewContent(contentBody);
+    const newContentString = JSON.stringify(newContent, null, '\t');
+    const oldContentString = await this.readOldContent(this.name);
+    if (oldContentString === "") {
+      this.writeOldContent(this.name, newContentString);
+      return [];
+    }
+    const diff = this.diffNewsJson(JSON.parse(oldContentString), newContent);
+    if (diff.length > -1) {
+      fs.writeFileSync(this.name, newContentString, 'utf-8');
+    }
+    return diff;
+  }
+}
